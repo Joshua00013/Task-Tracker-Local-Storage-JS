39a56eb3e15433a9530d8cbe1c52e4473c5fbde5
Joshua <149653102+Joshua00013@users.noreply.github.com>
2024-11-18 21:08:00 +0800

    Uploaded main files

---
+++ index.js
@@ -0,0 +1,69 @@
+
+function submit(){
+    const taskName = document.getElementById('taskInput_el').value;
+    const newTask = {
+        task: taskName,
+        completed: false
+    }
+    localStorage.setItem(newTask.task,newTask.completed);
+    renderTasks();
+}
+
+function renderTasks(){
+    const tasklist = document.getElementById("taskList_el");
+    tasklist.innerHTML = '';
+
+    for (let i = 0; i < localStorage.length; i++){
+        const taskName = document.createElement("p");
+        const taskItem = document.createElement("li");
+        const checkbox = document.createElement("input");
+        const deleteButton = document.createElement("button");
+        const deleteIcon = document.createElement("img")
+
+        deleteIcon.src= "delete_24dp_000000_FILL0_wght400_GRAD0_opsz24.png"
+        deleteButton.className = "deletebtn";
+        taskItem.className = "taskitem";
+        checkbox.type = "checkbox";
+        
+
+        const key = localStorage.key(i);
+        let status = JSON.parse(localStorage.getItem(key));
+        taskName.textContent = key;
+        console.log(status);
+        if(status !== false){
+            checkbox.checked = true;
+            checkbox.className = "checked"
+            taskName.className = "disabled"
+            console.log("DISABLED!")
+        }
+
+        checkbox.addEventListener("change", ()=>{
+            
+            if(status===false){
+                status = true;
+            }
+            else{
+                status =false;
+            }
+            localStorage.setItem(key, JSON.stringify(status));
+            renderTasks();
+        })
+
+        deleteButton.addEventListener("click",()=>{
+            taskItem.remove();
+            localStorage.removeItem(key)
+        })
+
+        deleteButton.appendChild(deleteIcon);
+        
+        taskItem.appendChild(checkbox);
+        taskItem.appendChild(taskName);
+        taskItem.appendChild(deleteButton);
+        tasklist.appendChild(taskItem)
+    }
+
+}
+
+
+
+renderTasks();
